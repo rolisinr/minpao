aab10125cd630c7469130594e0fba86ca44a3406
rolisinr <rolisilvestrerojas@gmail.com>
2026-07-08 22:05:58 -0500

    Cambios en el banner
    botones rediseñados

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'minpao-v39'; // subir SIEMPRE que cambie voz.js u otro asset
+const CACHE_NAME = 'minpao-v40'; // subir SIEMPRE que cambie voz.js u otro asset
 const ASSETS = [
   './',
   './index.html',
