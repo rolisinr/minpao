88839b51bb851942a9c9a61aefcbd53b721b20ad
rolisinr <rolisilvestrerojas@gmail.com>
2026-07-08 21:30:48 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'minpao-v38'; // subir SIEMPRE que cambie voz.js u otro asset
+const CACHE_NAME = 'minpao-v39'; // subir SIEMPRE que cambie voz.js u otro asset
 const ASSETS = [
   './',
   './index.html',
